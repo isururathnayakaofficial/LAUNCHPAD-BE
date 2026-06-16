53fc39a4e6f223b51d6552d8207348912d25aac4
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-06-16 21:53:25 +0530

    feat:db schima finalized for tasks assigning automate feature

---
+++ models/tasks.model.ts
@@ -0,0 +1,102 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+
+export interface ITaskDocument extends Document {
+
+    title:string;
+
+    email:string;
+
+    role:string;
+
+    description:string;
+
+    mediaUrl?: string[];
+
+    inviteToken:string;
+
+    assignedUserId?: mongoose.Types.ObjectId;
+
+    status:'pending' | 'in-progress' | 'completed';
+
+    createdAt:Date;
+
+    updatedAt:Date;
+}
+
+
+
+const taskSchema = new Schema<ITaskDocument>(
+
+{
+
+title:{
+    type:String,
+    required:true
+},
+
+
+email:{
+    type:String,
+    required:true,
+    lowercase:true
+},
+
+
+role:{
+    type:String,
+    required:true
+},
+
+
+description:{
+    type:String,
+    required:true
+},
+
+
+mediaUrl:[{
+    type:String
+}],
+
+
+
+inviteToken:{
+    type:String,
+    required:true,
+    unique:true
+},
+
+
+
+assignedUserId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+},
+
+
+
+status:{
+    type:String,
+    enum:[
+        'pending',
+        'in-progress',
+        'completed'
+    ],
+    default:'pending'
+}
+
+
+},
+
+{
+timestamps:true
+}
+
+);
+
+
+export default mongoose.model<ITaskDocument>(
+"Tasks",
+taskSchema
+);
