28d4882f7129137e8d90e225eadc77ad605a8c8d
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-06-16 11:58:52 +0530

    test:privet todo endpoint tests passed

--- routes/privateTodos.routers.ts
+++ routes/privateTodos.routers.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import{ createPrivateTodo } from "../controllers/privateTodos.controller";
-
+import { authMiddleware } from "../middleware/auth.middleware";
 const router = Router();
 
 //router.get("/getAll", getPrivateTodos);
-router.post("/save", createPrivateTodo);
+router.post("/save", authMiddleware, createPrivateTodo);
 
 export default router;
