20bd1386c5f1a189431f806efadbcbd0b132c814
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-06-22 12:34:23 +0530

    test:profile endpoint test passed

--- app.ts
+++ app.ts
@@ -16,12 +16,12 @@ import cors from "cors";
 
 
 const app: Application = express();
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true
+// }));
 app.options(/.*/, cors());
 
 app.use(express.json());
